6c2feebb54f6567bc8a64444b9a7dd7ff36d2d27
Vyacheslav Gulyaev <vv_gulyaev@mail.ru>
2016-12-03 09:40:56 +0300

    refactored 3C task

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ function getPokemonNames(input_array)
 	var pokemonNames = [];
 	for (var i=0; i<input_array.length; i++)
 	{
-		//console.log(input_array[i].name);
 		pokemonNames.push(input_array[i].name);
 	}
 	return pokemonNames;
@@ -30,126 +29,79 @@ function getPokemonWithFats(input_array)
 	return pokemonWithFats;
 }
 
+function getOutputIndexes(offset, limit)
+{
+	const defaultOffset = 0;
+	const defaultLimit = 20;
+	var cur_offset;
+	var cur_limit;
+	var indexes = {};
+
+	if (offset!==undefined)
+		cur_offset = (+offset || 0);
+	else
+		cur_offset = defaultOffset;
+
+	if (limit!==undefined)
+		cur_limit = (+limit || 0);
+	else
+		cur_limit = defaultLimit;
+
+	indexes['startIndex'] = cur_offset;
+	indexes['finishIndex'] = cur_offset + cur_limit;
+	console.log(indexes);
+	return indexes;
+}
 
 app.get('/task3C', async (req, res) => {
 	const sortPokemons = _.sortBy(pokemons, pokemon => pokemon.name);
 	const pokemonNames = getPokemonNames(sortPokemons);
-	var limit;
-	var offset;
-	if (req.query.limit!==undefined)
-		limit = (+req.query.limit || 0);
-	else
-		limit = 20;
-
-	if (req.query.offset!==undefined)
-		offset = (+req.query.offset || 0);
-	else
-		offset = 0;
-	return res.json(pokemonNames.slice(offset, offset + limit));
+	const outputIndexes = getOutputIndexes(req.query.offset, req.query.limit);
+	return res.json(pokemonNames.slice(outputIndexes.startIndex, outputIndexes.finishIndex));
 });
 
 app.get('/task3C/huge', async (req, res) => {
 	const sortPokemons = _.orderBy(pokemons, ['height', 'name'], ['desc', 'asc']);
 	const pokemonNames = getPokemonNames(sortPokemons);
-	var limit;
-	var offset;
-	if (req.query.limit!==undefined)
-		limit = (+req.query.limit || 0);
-	else
-		limit = 20;
-
-	if (req.query.offset!==undefined)
-		offset = (+req.query.offset || 0);
-	else
-		offset = 0;
-	return res.json(pokemonNames.slice(offset, offset + limit));
+	const outputIndexes = getOutputIndexes(req.query.offset, req.query.limit);
+	return res.json(pokemonNames.slice(outputIndexes.startIndex, outputIndexes.finishIndex));
 });
 
 app.get('/task3C/micro', async (req, res) => {
 	const sortPokemons = _.orderBy(pokemons, ['height', 'name'], ['asc', 'asc']);
 	const pokemonNames = getPokemonNames(sortPokemons);
-	var limit;
-	var offset;
-	if (req.query.limit!==undefined)
-		limit = (+req.query.limit || 0);
-	else
-		limit = 20;
-
-	if (req.query.offset!==undefined)
-		offset = (+req.query.offset || 0);
-	else
-		offset = 0;
-	return res.json(pokemonNames.slice(offset, offset + limit));
+	const outputIndexes = getOutputIndexes(req.query.offset, req.query.limit);
+	return res.json(pokemonNames.slice(outputIndexes.startIndex, outputIndexes.finishIndex));
 });
 
 app.get('/task3C/light', async (req, res) => {
 	const sortPokemons = _.orderBy(pokemons, ['weight', 'name'], ['asc', 'asc']);
 	const pokemonNames = getPokemonNames(sortPokemons);
-	var limit;
-	var offset;
-	if (req.query.limit!==undefined)
-		limit = (+req.query.limit || 0);
-	else
-		limit = 20;
-
-	if (req.query.offset!==undefined)
-		offset = (+req.query.offset || 0);
-	else
-		offset = 0;
-	return res.json(pokemonNames.slice(offset, offset + limit));
+	const outputIndexes = getOutputIndexes(req.query.offset, req.query.limit);
+	return res.json(pokemonNames.slice(outputIndexes.startIndex, outputIndexes.finishIndex));
 });
 
 app.get('/task3C/heavy', async (req, res) => {
 	const sortPokemons = _.orderBy(pokemons, ['weight', 'name'], ['desc', 'asc']);
 	const pokemonNames = getPokemonNames(sortPokemons);
-	var limit;
-	var offset;
-	if (req.query.limit!==undefined)
-		limit = (+req.query.limit || 0);
-	else
-		limit = 20;
-
-	if (req.query.offset!==undefined)
-		offset = (+req.query.offset || 0);
-	else
-		offset = 0;
-	return res.json(pokemonNames.slice(offset, offset + limit));
+	const outputIndexes = getOutputIndexes(req.query.offset, req.query.limit);
+	return res.json(pokemonNames.slice(outputIndexes.startIndex, outputIndexes.finishIndex));
 });
 
 app.get('/task3C/angular', async (req, res) => {
 	const PokemonsWithFat = getPokemonWithFats(pokemons);
 	const sortPokemons = _.orderBy(PokemonsWithFat, ['fat', 'name'], ['asc', 'asc']);
 	const pokemonNames = getPokemonNames(sortPokemons);
-	var limit;
-	var offset;
-	if (req.query.limit!==undefined)
-		limit = (+req.query.limit || 0);
-	else
-		limit = 20;
-
-	if (req.query.offset!==undefined)
-		offset = (+req.query.offset || 0);
-	else
-		offset = 0;
-	return res.json(pokemonNames.slice(offset, offset + limit));
+	const outputIndexes = getOutputIndexes(req.query.offset, req.query.limit);
+	return res.json(pokemonNames.slice(outputIndexes.startIndex, outputIndexes.finishIndex));
 });
 
 app.get('/task3C/fat', async (req, res) => {
 	const PokemonsWithFat = getPokemonWithFats(pokemons);
 	const sortPokemons = _.orderBy(PokemonsWithFat, ['fat', 'name'], ['desc', 'asc']);
 	const pokemonNames = getPokemonNames(sortPokemons);
-	var limit;
-	var offset;
-	if (req.query.limit!==undefined)
-		limit = (+req.query.limit || 0);
-	else
-		limit = 20;
-
-	if (req.query.offset!==undefined)
-		offset = (+req.query.offset || 0);
-	else
-		offset = 0;
-	return res.json(pokemonNames.slice(offset, offset + limit));
+	const outputIndexes = getOutputIndexes(req.query.offset, req.query.limit);
+	return res.json(pokemonNames.slice(outputIndexes.startIndex, outputIndexes.finishIndex));
 });
 
 app.listen(3000, () => {
